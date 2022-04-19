92d795bff1ea62403c1dda04848d7971ea40e4ea
Pedro Santos <39107716+pedrosantos53@users.noreply.github.com>
2022-04-18 21:14:27 -0700

    Fixed wrong Lua syntax and slight optimization
    Fixed invalid usage of compound operator "-=" and refactored code to use table.remove and ipairs rather than table[k] = nil and pairs

--- timer.lua
+++ timer.lua
@@ -46,16 +46,16 @@ function timer.Update(dt)
 				if v.repetitions ~= 0 and v.repetitionsDone >= v.repetitions then
 					timer.namedTimers[k] = nil
 				else
-					v.delayTimer -= v.delay
+					v.delayTimer = v.delayTimer - v.delay
 				end
 			end
 		end
 	end
-	for k,v in pairs(timer.simpleTimers) do
+	for k,v in ipairs(timer.simpleTimers) do
 		v.delayTimer = v.delayTimer + dt
 		if v.delayTimer >= v.delay then
 			v.func()
-			timer.simpleTimers[k]=nil
+			table.remove(timer.simpleTimers, k)
 		end
 	end
 end --[[internal: go through all functions and do whatever is done]]
